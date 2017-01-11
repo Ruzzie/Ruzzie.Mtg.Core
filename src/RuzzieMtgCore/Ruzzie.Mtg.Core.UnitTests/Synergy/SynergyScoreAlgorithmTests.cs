b1716e09c069ff962f4e4f32f261fe25a4f309e4
Dorus Verhoeckx <dorus.verhoeckx@gmail.com>
2017-01-11 09:56:19 +0100

    - Added SynergyScoreAlgorithm type

---
+++ src/RuzzieMtgCore/Ruzzie.Mtg.Core.UnitTests/Synergy/SynergyScoreAlgorithmTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Ruzzie.Mtg.Core.Synergy;
+
+namespace Ruzzie.Mtg.Core.UnitTests.Synergy
+{
+    [TestFixture]
+    public class SynergyScoreAlgorithmTests
+    {
+        [TestCase(0.000009F, 0.99999F, 0.99999F)]
+        [TestCase(0F, 0.99999F, 0.99999F)]
+        [TestCase(0.99999F, 0.99999F, 1.24999F)]
+        [TestCase(0.99999F, 0F, 0.99999F)]
+        [TestCase(0.01F, 1F, 1.00999999f)]
+        [TestCase(0F, 1F, 1F)]
+        public void TestScoreIncrement(float score, float increment, float expected)
+        {
+            Assert.That(score.IncrementSynergyScore(increment), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void VerySmallScoreValueShouldNotReturnLargeReturnValue()
+        {
+            Assert.That(0.0001F.IncrementSynergyScore(), Is.LessThanOrEqualTo(1.0001F));
+        }
+
+        [Test]
+        public void ScoreCannotBeGreaterThat10()
+        {
+            Assert.That(10F.IncrementSynergyScore(), Is.LessThanOrEqualTo(10F));
+        }
+    }
+}
---
+++ src/RuzzieMtgCore/Ruzzie.Mtg.Core/Synergy/SynergyScoreAlgorithm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Ruzzie.Mtg.Core.Synergy
+{
+    /// <summary>
+    /// Contains methods to assign and increment a score for card synergy.
+    /// </summary>
+    public static class SynergyScoreAlgorithm
+    {
+        /// <summary>
+        /// The epsilon
+        /// </summary>
+        public const float Epsilon = 0.00001F;
+
+        /// <summary>
+        /// Determines whether this instance is zero whitin the tolerance of <see cref="Epsilon"/>
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified value is zero; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsZero(this float value)
+        {
+            return Math.Abs(value) < Epsilon;
+        }
+
+        /// <summary>
+        /// Increments the synergy score.
+        /// </summary>
+        /// <param name="currentScore">The current score.</param>
+        /// <param name="incrementFactor">The increment factor.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">
+        /// incrementFactor should be a value between 0 and 1. Current value is: {incrementFactor}
+        /// or
+        /// Increment should be a value between 0 and 1. Current value is: {incrementFactor}
+        /// </exception>
+        public static float IncrementSynergyScore(this float currentScore, float incrementFactor = 1)
+        {
+            if (incrementFactor > 1)
+            {
+                throw new ArgumentException($"incrementFactor should be a value between 0 and 1. Current value is: {incrementFactor}", nameof(incrementFactor));
+            }
+
+            if (incrementFactor < 0)
+            {
+                throw new ArgumentException($"Increment should be a value between 0 and 1. Current value is: {incrementFactor}", nameof(incrementFactor));
+            }
+
+            if (currentScore.IsZero())
+            {
+                return incrementFactor;
+            }
+
+            if (incrementFactor.IsZero())
+            {
+                return currentScore;
+            }
+
+            float totalIncrement = ((incrementFactor * 0.25F) / currentScore);
+
+            if (totalIncrement > incrementFactor)
+            {
+                return currentScore + incrementFactor;
+            }
+
+            float newScore = currentScore;
+
+            if (currentScore < 10)
+            {
+                newScore = currentScore + totalIncrement;
+            }
+
+            return newScore;
+        }
+    }
+}
